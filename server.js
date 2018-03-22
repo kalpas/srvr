36eb4e9b8de6c3593b9ad2a63336c32a8aeb6c4a
kalpas <andreii.arnautov@gmail.com>
2018-03-22 15:43:16 +0200

    body

--- server.js
+++ server.js
@@ -2,23 +2,25 @@ http = require('http');
 fs = require('fs');
 shell = require('shelljs');
 server = http.createServer(function(req, res) {
+    let body = [];
+    request.on('error', (err) => {
+        console.error(err);
+    }).on('data', (chunk) => {
+        body.push(chunk);
+    }).on('end', () => {
+        body = Buffer.concat(body).toString();
+        if (body){
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end('post received');
+            shell.exec('~/Mipow-Playbulb-BTL201/mipow.exp night '+ body)
+        }else{
+            shell.exec('~/Mipow-Playbulb-BTL201/mipow.exp night toggle')
+        }
 
-    console.dir(req.param);
+        
+    });
 
-    if (req.method == 'POST') {
-        console.log("POST");
-        var body = '';
-        req.on('data', function(data) {
-            body += data;
-            console.log("Partial body: " + body);
-        });
-        req.on('end', function() {
-            console.log("Body: " + body);
-        });
-        res.writeHead(200, { 'Content-Type': 'application/json' });
-        res.end('post received');
-        shell.exec('~/Mipow-Playbulb-BTL201/mipow.exp night color 0 255 0 0')
-    }
+   
 
 });
 
