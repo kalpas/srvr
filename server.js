0ee5c26450e14a3b51083d287cdeb90bcb6e071b
kalpas <andreii.arnautov@gmail.com>
2018-03-22 15:45:16 +0200

    body

--- server.js
+++ server.js
@@ -3,25 +3,21 @@ fs = require('fs');
 shell = require('shelljs');
 server = http.createServer(function(req, res) {
     let body = [];
-    request.on('error', (err) => {
+    req.on('error', (err) => {
         console.error(err);
     }).on('data', (chunk) => {
         body.push(chunk);
     }).on('end', () => {
         body = Buffer.concat(body).toString();
+        console.log(body);
         if (body){
             res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end('post received');
             shell.exec('~/Mipow-Playbulb-BTL201/mipow.exp night '+ body)
         }else{
             shell.exec('~/Mipow-Playbulb-BTL201/mipow.exp night toggle')
-        }
-
-        
+        }  
     });
-
-   
-
 });
 
 port = 9000;
